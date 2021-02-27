43c8e933377791b556449c93c4ac4fc6b296b432
Stathis Dimitriadis <stathisdim@gmail.com>
2021-02-27 20:15:57 +0200

    Add second waves

---
+++ Group24Exe7Prog1.m
@@ -0,0 +1,88 @@
+%Efstathios Dimitriadis 8490
+%Emmanouil Mylonas 9508
+% Define second wave for each country
+Group24Exe1Prog3
+
+%% United Kingdom
+second_cases_uk = total_cases_uk(240:348);
+normalised_second_cases_uk = second_cases_uk/sum(second_cases_uk);
+
+second_deaths_uk = total_deaths_uk(272:348);
+normalised_second_deaths_uk = second_deaths_uk/sum(second_deaths_uk);
+
+%% Austria
+second_cases_au = total_cases_au(223:348);
+normalised_second_cases_au = second_cases_au/sum(second_cases_au);
+
+second_deaths_au = total_deaths_au(277:348);
+normalised_second_deaths_au = second_deaths_au/sum(second_deaths_au);
+
+%% Belgium
+second_cases_be = total_cases_bel(250:347);
+normalised_second_cases_be = second_cases_be/sum(second_cases_be);
+
+second_deaths_be = total_deaths_bel(270:347);
+normalised_second_deaths_be = second_deaths_be/sum(second_deaths_be);
+
+%% Denmark
+second_cases_de = total_cases_den(220:348);
+normalised_second_cases_de = second_cases_de/sum(second_cases_de);
+
+second_deaths_de = total_deaths_den(254:348);
+normalised_second_deaths_de = second_deaths_de/sum(second_deaths_de);
+
+%% France
+second_cases_fr = total_cases_fr(209:348);
+normalised_second_cases_fr = second_cases_fr/sum(second_cases_fr);
+
+second_deaths_fr = total_deaths_fr(255:348);
+normalised_second_deaths_fr = second_deaths_fr/sum(second_deaths_fr);
+
+%% Germany
+second_cases_ge = total_cases_ge(222:348);
+normalised_second_cases_ge = second_cases_ge/sum(second_cases_ge);
+
+second_deaths_ge = total_deaths_ge(267:348);
+normalised_second_deaths_ge = second_deaths_ge/sum(second_deaths_ge);
+
+%% Greece
+second_cases_gr = total_cases_gr(200:346);
+normalised_second_cases_gr = second_cases_gr/sum(second_cases_gr);
+
+second_deaths_gr = total_deaths_gr(238:346);
+normalised_second_deaths_gr = second_deaths_gr/sum(second_deaths_gr);
+
+%% Ireland
+second_cases_ir = total_cases_ir(200:346);
+normalised_second_cases_ir = second_cases_ir/sum(second_cases_ir);
+
+second_deaths_ir = total_deaths_ir(250:346);
+normalised_second_deaths_ir = second_deaths_ir/sum(second_deaths_ir);
+
+%% Italy
+second_cases_it = total_cases_it(230:348);
+normalised_second_cases_it = second_cases_it/sum(second_cases_it);
+
+second_deaths_it = total_deaths_it(279:348);
+normalised_second_deaths_it = second_deaths_it/sum(second_deaths_it);
+
+%% Russia
+second_ru = total_cases_ru(246:348);
+normalised_second_ru = second_ru/sum(second_ru);
+
+second_deaths_ru = total_deaths_ru(252:348);
+normalised_second_deaths_ru = second_deaths_ru/sum(second_deaths_ru);
+
+%% Sweden
+second_cases_sw = total_cases_sw(257:345);
+normalised_second_cases_sw = second_cases_sw/sum(second_cases_sw);
+
+second_deaths_sw = total_deaths_sw(295:345);
+normalised_second_deaths_sw = second_deaths_sw/sum(second_deaths_sw);
+
+%% Switzerland
+second_cases_swit = total_cases_swit(282:346);
+normalised_normalised_cases_swit = second_cases_swit/sum(second_cases_swit);
+
+second_deaths_swit = total_deaths_swit(300:346);
+normalised_second_deaths_swit = second_deaths_swit/sum(second_deaths_swit);
