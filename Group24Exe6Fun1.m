881c1fde4b100036dbcbb60f452b4c6f1d82c8fc
Emmanuel Mylonas <64604014+Thunderlord19@users.noreply.github.com>
2021-02-27 18:52:15 +0200

    Executive 6, needs check

---
+++ Group24Exe6Fun1.m
@@ -0,0 +1,59 @@
+%Efstathios Dimitriadis 8490
+%Emmanouil Mylonas 9508
+
+function [model, b, b2] = Group24Exe6Fun1(total_cases, total_deaths, start_cases, end_cases, adjR2_simple,name)
+   
+    adjRsq = @(ypred,y,n,k) ( 1 - (n-1)/(n-1-k)*sum((ypred-y).^2)/sum((y-mean(y)).^2) );
+    deaths_sample = total_deaths(start_cases:end_cases)/sum(total_deaths(start_cases:end_cases));
+    n = length(deaths_sample);
+    cases_sample = zeros(n,21);
+
+    for t=0:20
+        cases_sample(:,t+1) = total_cases(start_cases-t:end_cases-t);
+        cases_sample(:,t+1) = cases_sample(:,t+1)/sum(cases_sample(:,t+1));
+    end
+    %Stepwise Regression
+    [b,~,~,model,stats] = stepwisefit(cases_sample,deaths_sample','display','Off');
+    b0 = stats.intercept;
+    b = [b0; b(model)]; 
+
+    Ypred = [ones(length(cases_sample),1) cases_sample(:,model)]*b;
+    errors = deaths_sample' - Ypred;
+    rms_step = stats.rmse;
+    rmse_step = errors/rms_step;
+    adjR2_step = adjRsq(Ypred,deaths_sample',n,length(b));
+
+    figure()
+    scatter(Ypred,rmse_step)
+    hold on
+    plot(xlim,-1.96*[1 1],'--c')
+    plot(xlim,1.96*[1 1],'--c')
+    title('Diagnostic plot - Stepwise Regression - UK');
+
+    %Full 21 Model
+    regressionModel = fitlm(cases_sample,deaths_sample');
+    b2 = table2array(regressionModel.Coefficients);
+    b2 = b2(:,1);
+
+    ypred = [ones(n,1) cases_sample]*b2;
+    errors = deaths_sample' - ypred;
+    rms_full = sqrt( 1/(n-length(b2))*sum(errors).^2 );
+    rmse_full = errors/rms_full;
+    adjR2_full = adjRsq(ypred,deaths_sample',n,length(b2));
+
+    figure()
+    scatter(ypred,rmse_full)
+    hold on
+    plot(xlim,-1.96*[1 1],'--c')
+    plot(xlim,1.96*[1 1],'--c')
+    %This doesn't work, I dont know why there is a problem with title.
+    %title(sprintf('Diagnostic plot - Full Model - %s', name));
+
+    if adjR2_simple >= adjR2_step && adjR2_simple>= adjR2_full
+        fprintf('The best model for %s is simple linear regression\n',name)
+    elseif adjR2_step> adjR2_simple && adjR2_step >= adjR2_full
+        fpritf('The best model for %s is Stepwise Regression, with .0%f variables\n',name, length(b))
+    else
+        fprintf('The best model for %s is the full 21-variables model\n',name)
+    end
+end
--- Group24Exe6Prog1.m
+++ Group24Exe6Prog1.m
@@ -11,48 +11,106 @@ adjR2_simple = zeros(21,1);
 adjR2_multiple = zeros(21,2);
 adjR2_step = zeros(21,1);
 order = 0;
-e_standard_simple = zeros(length(normalised_cases_uk),21);
-y = zeros(length(normalised_cases_uk),21);
-b = zeros(2,21);
+n = length(normalised_cases_uk);
+rmse_linear = zeros(n,21);
+rmse_multiple = zeros(n,21);
+rmse_step = zeros(n,21);
+y_linear = zeros(n,21);
+y_multiple = zeros(n,21);
+y_step = zeros(n,21);
+b_linear = zeros(2,21);
+b_multiple = zeros(5,21);
+b_step = zeros(2,21);
+
 
 for t=0:20
     deaths_sample = total_deaths_uk(50+t:210+t)/sum(total_deaths_uk(50+t:210+t)); 
     cases_sample = [ones(size(normalised_cases_uk')) normalised_cases_uk'];
     
     %Simple regression
-    b(:,t+1) = regress(deaths_sample', cases_sample);
-    n = length(deaths_sample);
-    y(:,t+1) = cases_sample*b(:,t+1);
-    adjR2_simple(t+1) = adjRsq(y(:,t+1),deaths_sample',n,1);
-    se2 = sum((deaths_sample'-y(:,t+1)).^2/(n-2));
-    e_standard_simple(:,t+1) = (deaths_sample'-y(:,t+1))/sqrt(se2);
+    b_linear(:,t+1) = regress(deaths_sample', cases_sample);
+    y_linear(:,t+1) = cases_sample*b_linear(:,t+1);
+    adjR2_simple(t+1) = adjRsq(y_linear(:,t+1),deaths_sample',n,1);
+    se2 = sum((deaths_sample'-y_linear(:,t+1)).^2/(n-2));
+    rmse_linear(:,t+1) = (deaths_sample'-y_linear(:,t+1))/sqrt(se2);
     
     %Multiple linear regression (maybe not correct term :P)
     
     maxadjR2_linear = 0;
-    for k=1:4 %order of our regression - up to 4 because after that we receive a warning for polynomial being badly conditioned
+    rmse_local = zeros(n,4);
+    for k=2:4 %order of our regression - up to 4 because after that we receive a warning for polynomial being badly conditioned
         b2 = polyfit(normalised_cases_uk,deaths_sample,k);
         ypred = polyval(b2,normalised_cases_uk);
         adjR2 = adjRsq(ypred,deaths_sample,n,k);
+        se2 = sum((deaths_sample-ypred).^2/(n-2));
+        rmse_local(:,k) = (deaths_sample-ypred)/sqrt(se2);
         if adjR2 > maxadjR2_linear
             order = k;
             maxadjR2_linear = adjR2;
+            y_multiple(:,t+1) = ypred;
+            if k==2
+                b_multiple(1:3,t+1) = b2;
+            elseif k==3
+                b_multiple(1:4,t+1) = b2;
+            else
+                b_multiple(:,t+1) = b2;
+            end
+            
         end
     end
     adjR2_multiple((t+1),1) = maxadjR2_linear;
     adjR2_multiple((t+1),2) = order;
+    rmse_multiple(:,t+1) = rmse_local(:,order);
     
     % Step wise regression
     [b3,~,~,model,stats] = stepwisefit(normalised_cases_uk',deaths_sample','display','Off');
     b0 = stats.intercept;
     b3 = [b0; b3(model)]; 
-    Ypred = cases_sample*b3;
-    adjR2_step(t+1) = adjRsq(Ypred, deaths_sample', n, length(b));
+    y_step(:,t+1) = cases_sample*b3;
+    adjR2_step(t+1) = adjRsq(y_step(:,t+1), deaths_sample', n, length(b_linear));
+    se2 = sum((deaths_sample'-y_step(:,t+1)).^2/(n-2));
+    rmse_step(:,t+1) = (deaths_sample'-y_step(:,t+1))/sqrt(se2);
+    b_step(:,t+1) = b3;
 
 end
 
-% [value1,pos1] = max(adjR2_simple);
-% [value2, pos2] = max(adjR2_multiple;
+%RESULTS
+
+[value1,pos1] = max(adjR2_simple);
+figure()
+scatter(y_linear(:,pos1),rmse_linear(:,pos1))
+hold on
+plot(xlim,-1.96*[1 1],'--c')
+plot(xlim,1.96*[1 1],'--c')
+title('Diagnostic plot - simple Regression - UK');
+fprintf('Linear Regression -  UK: y = (%f) * x + (%f)\n',b_linear(2,pos1),b_linear(1,pos1))
+fprintf('Linear Regression - UK - delay = %.0f\n',pos1-1)
+fprintf('Linear Regression - UK - Adjusted R = %f\n',value1)
+
+
+%for the next two I have to find the model as well
+[value2, pos2] = max(adjR2_multiple(:,1));
+figure()
+scatter(y_multiple(:,pos2),rmse_multiple(:,pos2))
+hold on
+plot(xlim,-1.96*[1 1],'--c')
+plot(xlim,1.96*[1 1],'--c')
+title('Diagnostic plot - multiple Regression - UK');
+fprintf('Multiple Regression - UK: y = (%f) + (%f)*x + (%f)*x^2 + (%f)*x^3 + (%f)*x^4\n',b_multiple(1,pos2),b_multiple(2,pos2),b_multiple(3,pos2),b_multiple(4,pos2),b_multiple(5,pos2))
+fprintf('Multiple Regression - UK - delay = %.0f\n',pos2-1)
+fprintf('Multiple Regression - UK - Adjusted R = %f\n',value2)
+
+[value3,pos3] = max(adjR2_step);
+figure()
+scatter(y_step(:,pos3),rmse_step(:,pos3))
+hold on
+plot(xlim,-1.96*[1 1],'--c')
+plot(xlim,1.96*[1 1],'--c')
+title('Diagnostic plot - Stepwise Regression - UK');
+fprintf('Stepwise Regression -  UK: y = (%f) * x + (%f)\n',b_step(2,pos3),b_step(1,pos3))
+fprintf('Stepwise Regression - UK - delay = %.0f\n',pos3-1)
+fprintf('Stepwise Regression - UK - Adjusted R = %f\n',value3)
+
 
 
 %rest of the countries need to be added and reach conclusion, other than
